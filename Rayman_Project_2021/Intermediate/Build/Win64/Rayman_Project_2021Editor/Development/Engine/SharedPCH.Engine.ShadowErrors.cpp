016023c0b7ae50af21b1fe01ee75249270a5e2e0
R8Prod <44554413+RaphGamerProd@users.noreply.github.com>
2021-04-08 13:05:38 +0200

    Delete Rayman_Project_2021 directory

--- Rayman_Project_2021/Intermediate/Build/Win64/Rayman_Project_2021Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.24.28314
-#include "C:/Users/RaphGamerProd/Documents/GitHub/NSI-Rayman-2021/Rayman_Project_2021/Intermediate/Build/Win64/Rayman_Project_2021Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Rayman_Project_2021/Source/Rayman_Project_2021/Rayman_Project_2021.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Rayman_Project_2021.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rayman_Project_2021, "Rayman_Project_2021" );
